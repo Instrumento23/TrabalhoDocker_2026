0a621cedacf5c2e285d14d80d2c4ac0809ba9cc4
Instrumento23 <hateinstrument@gmail.com>
2026-06-19 19:07:23 -0300

    Delete trabalhoDocker directory

--- trabalhoDocker/projeto-tabela-periodica-main/api/src/main/java/periodicfive/api/repositories/ElementoRepository.java
+++
@@ -1,7 +0,0 @@
-package periodicfive.api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import periodicfive.api.model.Elemento;
-
-public interface ElementoRepository extends JpaRepository<Elemento, Integer> {
-}
